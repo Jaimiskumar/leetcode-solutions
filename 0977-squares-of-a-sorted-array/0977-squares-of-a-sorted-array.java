1da16bba89a6b6ac5a95892e88d1e3a04f10bc4b
Gondaliya Jaimiskumar Rajendrabhai <jaimiskumargondaliya@gmail.com>
2026-06-29 12:22:10 +0530

    Time: 5 ms (41.3%), Space: 48.8 MB (9.78%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,16 +1,11 @@
 import java.util.ArrayList;
 import java.util.Collections;
-
 class Solution {
     public int[] sortedSquares(int[] nums) {
-
         ArrayList<Integer> arr1 = new ArrayList<>();
         ArrayList<Integer> arr2 = new ArrayList<>();
         ArrayList<Integer> res = new ArrayList<>();
-
         int i, j, k;
-
-        // split negative and non-negative
         for (i = 0; i < nums.length; i++) {
             if (nums[i] < 0) {
                 arr1.add(nums[i]);
@@ -18,24 +13,15 @@ class Solution {
                 arr2.add(nums[i]);
             }
         }
-
-        // square negatives
         for (j = 0; j < arr1.size(); j++) {
             arr1.set(j, arr1.get(j) * arr1.get(j));
         }
-
         Collections.reverse(arr1);
-
-        // square positives
         for (k = 0; k < arr2.size(); k++) {
             arr2.set(k, arr2.get(k) * arr2.get(k));
         }
-
-        // reset pointers
         j = 0;
         k = 0;
-
-        // merge
         while (j < arr1.size() && k < arr2.size()) {
             if (arr1.get(j) <= arr2.get(k)) {
                 res.add(arr1.get(j));
@@ -45,23 +31,18 @@ class Solution {
                 k++;
             }
         }
-
         while (k < arr2.size()) {
             res.add(arr2.get(k));
             k++;
         }
-
         while (j < arr1.size()) {
             res.add(arr1.get(j));
             j++;
         }
-
-        // convert ArrayList to array
         int[] ans = new int[res.size()];
         for (i = 0; i < res.size(); i++) {
             ans[i] = res.get(i);
         }
-
         return ans;
     }
 }

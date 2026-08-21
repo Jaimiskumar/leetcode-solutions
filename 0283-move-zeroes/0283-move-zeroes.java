a4892fed17730557ecd6bd663f61c8bc0e8435d9
Gondaliya Jaimiskumar Rajendrabhai <jaimiskumargondaliya@gmail.com>
2026-08-21 09:55:37 +0530

    Time: 2 ms (91.97%), Space: 47.6 MB (88.62%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,23 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         if (nums.length == 0) return;
-        int i = 0;
-        int j = 0;
-        while (j < nums.length) {
-            if (nums[j] != 0) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
+        // while (j < nums.length) {
+        //     if (nums[j] != 0) {
+        //         int temp = nums[i];
+        //         nums[i] = nums[j];
+        //         nums[j] = temp;
+        //         i++;
+        //     }
+        //     j++;
+        // }
+        int idx=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[idx++]=nums[i];
             }
-            j++;
+        }
+        for(int j=idx;j<nums.length;j++){
+            nums[idx++]=0;
         }
     }
 }

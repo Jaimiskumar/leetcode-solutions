b364aba1c443ffc2a9016ddc25d7e1eb8c5dfa45
Gondaliya Jaimiskumar Rajendrabhai <jaimiskumargondaliya@gmail.com>
2026-09-09 12:24:19 +0530

    Time: 3 ms (94.14%), Space: 46.6 MB (16.14%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        boolean ans=false;
+        if(s.length()!=t.length()){
+            return ans;
+        }else{
+            char[] a=s.toCharArray();
+            char[] b=t.toCharArray();
+            Arrays.sort(a);
+            Arrays.sort(b);
+            if(Arrays.equals(a,b)){
+                ans=true;
+            }
+        }
+        return ans;
+    }
+}

276e463e69af7f9d4a952cc998dda8defc13294c
Gondaliya Jaimiskumar Rajendrabhai <jaimiskumargondaliya@gmail.com>
2026-09-16 11:50:02 +0530

    Time: 0 ms (100%), Space: 44.4 MB (30.84%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,31 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         if(head == null){
+//             return null;
+//         }
+//         ArrayList<Integer> list = new ArrayList<>(); 
+//         ListNode temp = head; 
+//         while(temp!=null){ 
+//             list.add(temp.val); 
+//             temp=temp.next; 
+//         } 
+//         int[] arr1=new int[list.size()]; 
+//         for(int i=0;i<list.size();i++){ 
+//             arr1[i]=list.get(i); 
+//         } 
+//         int[] arr2=new int[arr1.length];
+//         for(int i=0;i<arr1.length;i++){ 
+//             arr2[i]=arr1[arr1.length-1-i]; 
+//         }
+//         ListNode newHead = new ListNode(arr2[0]);
+//         ListNode curr = newHead;
+//         for(int i = 1; i < arr2.length; i++) {
+//             curr.next = new ListNode(arr2[i]);
+//             curr = curr.next;
+//         }
+//         return newHead;
+//     }
+// }
+
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head == null){
-            return null;
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
         }
-        ArrayList<Integer> list = new ArrayList<>(); 
-        ListNode temp = head; 
-        while(temp!=null){ 
-            list.add(temp.val); 
-            temp=temp.next; 
-        } 
-        int[] arr1=new int[list.size()]; 
-        for(int i=0;i<list.size();i++){ 
-            arr1[i]=list.get(i); 
-        } 
-        int[] arr2=new int[arr1.length];
-        for(int i=0;i<arr1.length;i++){ 
-            arr2[i]=arr1[arr1.length-1-i]; 
-        }
-        ListNode newHead = new ListNode(arr2[0]);
-        ListNode curr = newHead;
-        for(int i = 1; i < arr2.length; i++) {
-            curr.next = new ListNode(arr2[i]);
-            curr = curr.next;
-        }
-        return newHead;
+        return prev;
     }
 }
